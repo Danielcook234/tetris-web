ab3906bcb36f5927b77a243bceb954f0f0959737
DCook18 <danieladam.cook@icloud.com>
2025-05-29 19:44:05 +0100

    added button to swap between start page and gameplay page

--- tetris/tetris_game/views.py
+++ tetris/tetris_game/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'tetris_game/index.html')
+
+def start_btn(request):
+    return render(request, 'tetris_game/game.html')
